43df2b92141cabe7472a3a5342c00ab75c3cb538
Miles Frain <miles.frain@colorado.edu>
2020-09-16 08:05:32 -0700

    try 3 with output capture

--- ci.sh
+++ ci.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-pwd
-
-ls -a
-
 ./format/check.sh
 /opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build all -data .
 # todo unit testing
