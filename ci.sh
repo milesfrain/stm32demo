e875e91f2467b566dac37e4f213662fa40ba3eb3
Miles Frain <miles.frain@colorado.edu>
2020-09-15 21:56:46 -0700

    add ci script

---
+++ ci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+./format/check.sh
+/opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build all -data .
+# todo unit testing
