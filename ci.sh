ee32050815126c9db4e461f4f50a8fecdd7de753
Miles Frain <miles.frain@colorado.edu>
2020-09-16 09:02:56 -0700

    try4 more error and stdout troubleshooting

--- ci.sh
+++ ci.sh
@@ -1,7 +1,24 @@
 #!/bin/sh -l
 
-set -ex
+# Was hoping that this would be enough to reliably exit on failure
+#set -ex
 
 ./format/check.sh
-/opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build all -data .
+format_ret=$?
+
+#/opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build all -data . 2>&1
+
+# version that definitely fails
+/opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild 2>&1
+build_ret=$?
+
 # todo unit testing
+
+echo format return $format_ret
+
+echo build return $build_ret
+
+status=$((format_ret || build_ret))
+echo status $status
+
+exit $status
