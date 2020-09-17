b5754364a236b468a1f146e9ee30525486e5e7a3
Miles Frain <miles.frain@colorado.edu>
2020-09-16 17:14:45 -0700

    Simplify ci.sh for forum

--- ci.sh
+++ ci.sh
@@ -1,32 +1,11 @@
 #!/bin/sh -l
 
-# Was hoping that this would be enough to reliably exit on failure
-#set -ex
-
-# just echo commands
-set -x
-
-./format/check.sh
-format_ret=$?
-
-echo listing directory
-ls
-
-echo running build
+set -ex
 
 /opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build all -data . 2>&1
 
-# version that definitely fails
-#/opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild 2>&1
-build_ret=$?
+rtval=$?
 
-# todo unit testing
+echo exit status: $rtval
 
-echo format return $format_ret
-
-echo build return $build_ret
-
-status=$((format_ret || build_ret))
-echo status $status
-
-exit $status
+exit $rtval
