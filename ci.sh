f7efc6203e2ef2f625e4c80a035db58fcfe814d9
Miles Frain <miles.frain@colorado.edu>
2020-09-21 14:06:18 -0700

    simplify ci.sh

--- ci.sh
+++ ci.sh
@@ -9,15 +9,8 @@ set -x
 ./format/check.sh
 format_ret=$?
 
-echo listing directory
-ls
+/opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data . -importAll . -build all -no-indexer
 
-echo running build
-
-/opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data . -importAll . -build all -no-indexer 2>&1
-
-# version that definitely fails
-#/opt/st/stm32cubeide_1.4.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild 2>&1
 build_ret=$?
 
 # todo unit testing
